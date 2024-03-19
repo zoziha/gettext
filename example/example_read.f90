14f35f27d7d22ca92a5d21d96c1e43e75f4a07b0
ZUO Zhihua <zuo.zhihua@qq.com>
2024-03-19 23:22:42 +0800

    支持meson构建

--- example/example_read.f90
+++ example/example_read.f90
@@ -6,6 +6,7 @@ program example_read
 
     call read_table(tbl, './example/1.txt')
     print *, gettext&
-&(tbl,"XYZ                                                                                                 ")
+&(tbl, "XYZ                                                                                                 ")
+    print *, "你好"
 
 end program example_read
